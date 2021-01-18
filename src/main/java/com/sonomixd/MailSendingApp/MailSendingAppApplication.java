ed4c322a94f1d650e526eb9e99c9ad02ffe5e4d1
Sokol Ikonomi <sokol.ikonomi@linksmt.it>
2021-01-18 16:19:03 +0100

    first commit

---
+++ src/main/java/com/sonomixd/MailSendingApp/MailSendingAppApplication.java
@@ -0,0 +1,13 @@
+package com.sonomixd.MailSendingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSendingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSendingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonomixd/MailSendingApp/MailSendingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonomixd.MailSendingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSendingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
